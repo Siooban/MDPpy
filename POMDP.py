6e90fc5bcc701e09b4ee6202f78c95ce60ebccd4
Siooban <siooban@gmail.com>
2022-03-07 13:18:55 +0100

    'update'

--- POMDP.py
+++ POMDP.py
@@ -36,6 +36,65 @@ grille[3,7]=1
 print(grille)
 
 reward=np.zeros([5, 9])
-reward[3,3]=1
-reward[3,5]=-1
+reward[3,3]=10
+reward[3,5]=-100
 print(reward)
+
+action=["up","bottom", "left", "right"]
+
+
+#deterministic transition function
+def transitionFunctionD(states, action, currentState):
+    if action=="up":
+        stateUp=(currentState[0]-1, currentState[1])
+        if states[stateUp[0], stateUp[1]]==0:
+            return currentState
+        else:
+            return stateUp
+    if action=="bottom":
+        stateDown=(currentState[0]+1,currentState[1])
+        if states[stateDown[0],stateDown[1]]==0:
+            return currentState
+        else:
+            return stateDown
+    if action=="left":
+        stateLeft=(currentState[0], currentState[1]-1)
+        if states[stateLeft[0], stateLeft[1]]==0:
+            return currentState
+        else: 
+            return stateLeft
+    if action=="right":
+        stateRight=(currentState[0], currentState[1]+1)
+        if states[stateRight[0], stateRight[1]]==0:
+            return currentState
+        else: 
+            return stateRight
+        
+        
+print(transitionFunctionD(grille,"left", (1,3)))
+
+
+#stochastic transition function
+#observationFunction
+"""this function return the wall number surrounding a position"""
+def ObservationFunction(states, currentState):
+    wallDistribution=[]
+    stateUp=(currentState[0]-1, currentState[1])
+    if states[stateUp[0], stateUp[1]]==0:
+        wallDistribution.append("up")
+    stateDown=(currentState[0]+1,currentState[1])
+    if states[stateDown[0],stateDown[1]]==0:
+        wallDistribution.append("bottom")
+    stateLeft=(currentState[0], currentState[1]-1)
+    if states[stateLeft[0], stateLeft[1]]==0:
+        wallDistribution.append("left")
+    stateRight=(currentState[0], currentState[1]+1)
+    if states[stateRight[0], stateRight[1]]==0:
+        wallDistribution.append('right')
+    return wallDistribution
+    
+print(ObservationFunction(grille, (2,3)))
+print(ObservationFunction(grille, (1,1)))
+print(ObservationFunction(grille, (3,3)))
+def Game(states, reward, action):
+    return "victory"
